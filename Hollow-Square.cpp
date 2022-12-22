f0a3f411571795ec4d3893a1520a65fa31639ea8
Irina Stepanova <110852608+Irissha@users.noreply.github.com>
2022-12-22 19:41:55 +0500

    Add files via upload

---
+++ Hollow-Square.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	const char star = '*';
+	int size;
+	cout << "Введите размер стороны квадрата больше 2 : ";
+	cin >> size;
+	while (size < 3) {
+		cout << "\nВы вели число меньше 2: ";
+		cin >> size;
+	}
+	for (int i = 0; i < size; i++)
+		cout << star;
+	cout << endl;
+	for (int i = 0; i < size - 2; i++)
+		cout << star << setw(size - 1) << star << endl;
+	for (int i = 0; i < size; i++)
+		cout << star;
+	cout << endl;
+	return 0;
+}
---
+++ Subsequence.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream> 
+#include <math.h> 
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a, min, n;
+	n = 0;
+	cout << "\nÂâåäèòå ÷èñëî: ";
+	cin >> a;
+	while ((a == 0) && (n != 4))
+	{
+		cout << "\nÂû âåëè 0, ââåäèòå ÷èñëî ñíîâà: ";
+		cin >> a;
+		n++;
+	}
+	min = a;
+	while (a != 0)
+	{
+		cout << "\nÂâåäèòå ÷èñëî: ";
+		cin >> a;
+		if (a < min && a != 0)
+			min = a;
+	}
+	if (min == 0)
+	{
+		cout << "íóëåâàÿ ïîñëåäîâàòåëüíîñòü";
+	}
+	else {
+		cout << "min = " << min << endl;
+	}
+	return 0;
+}
