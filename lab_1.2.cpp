9874d4ad677212bf025ef435617ac8bd2df5ebc1
Irina Stepanova <110852608+Irissha@users.noreply.github.com>
2022-12-22 19:37:28 +0500

    Add files via upload

---
+++ lab_1.2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	float n, m, a, b, c;
+
+	setlocale(LC_ALL, "rus");
+
+	cout << "Введите два числа: ";
+	cin >> n;
+	cin >> m;
+	a = ++n * ++m;
+
+	cout << " ++n * ++m => " << a << "\n";
+
+	b = m++ < n;
+	cout << " m++ < n => " << b << "\n";
+
+	c = n++ > m;
+	cout << " n++ > m => " << c << "\n";
+}
